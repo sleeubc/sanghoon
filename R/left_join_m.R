967ada46ae0d8d0e8a15763374dc817aa9517196
Sanghoon Lee <sanghoon.lee@sauder.ubc.ca>
2017-12-23 22:57:22 -0800

    left_join_m.R debugged

--- R/left_join_m.R
+++ R/left_join_m.R
@@ -11,6 +11,25 @@
 #' band_members %>% left_join_m(band_instruments)
 #' band_members %>% left_join_m(band_instruments, by = "name")
 #' band_members %>% left_join_m(band_instruments2, by = c("name" = "artist"))
+#'
+#' https://stackoverflow.com/questions/26611717/can-dplyr-join-on-multiple-columns-or-composite-key
+#'
+#' library(dplyr)
+#' d1 <- data_frame(
+#'   x = letters[2:4],
+#'   y = LETTERS[2:4],
+#'   a = rnorm(3)
+#' )
+#'
+#' d2 <- data_frame(
+#'   x2 = letters[3:1],
+#'   y2 = LETTERS[3:1],
+#'   b = rnorm(3)
+#' )
+#'
+#' left_join_m(d1, d2, by = c("x" = "x2", "y" = "y2"))
+
+
 
 left_join_m <- function(df_left, df_right, by=NULL,... ) {
 
@@ -34,13 +53,13 @@ left_join_m <- function(df_left, df_right, by=NULL,... ) {
   )))
 
 
-  unmatched_left <- df_left %>% anti_join(outcome_df, by=key_left) %>% nrow()
-  message(paste0("Unmatched_left: ", unmatched_left))
+  unmatched_left <- df_left %>% anti_join(df_right, by) %>% nrow()
+  message(paste0("Unmatched rows (left): ", unmatched_left))
 
-  # temp <- key_left
-  # names(temp) <- key_right
-  # unmatched_right <- df_right %>% anti_join(outcome_df, by=temp) %>% nrow()
-  # message(paste0("unmatched_right: ", unmatched_right))
+  temp <- key_left
+  names(temp) <- key_right
+  unmatched_right <- df_right %>% anti_join(df_left, by=temp) %>% nrow()
+  message(paste0("unmatched_right: ", unmatched_right))
 
   outcome_df
 
