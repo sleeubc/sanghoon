8302869c0dce9baff276a85e0c9731f5a0281826
Sanghoon Lee <sanghoon.lee@sauder.ubc.ca>
2017-12-23 23:20:36 -0800

    left_join_m.R debugged #2

--- R/left_join_m.R
+++ R/left_join_m.R
@@ -12,23 +12,37 @@
 #' band_members %>% left_join_m(band_instruments, by = "name")
 #' band_members %>% left_join_m(band_instruments2, by = c("name" = "artist"))
 #'
-#' https://stackoverflow.com/questions/26611717/can-dplyr-join-on-multiple-columns-or-composite-key
+#' The following is based on https://stackoverflow.com/questions/26611717/can-dplyr-join-on-multiple-columns-or-composite-key
 #'
 #' library(dplyr)
-#' d1 <- data_frame(
-#'   x = letters[2:4],
-#'   y = LETTERS[2:4],
+#'
+#' d1 <- tibble(
+#'   x = letters[1:3],
+#'   y = LETTERS[1:3],
 #'   a = rnorm(3)
 #' )
 #'
-#' d2 <- data_frame(
-#'   x2 = letters[3:1],
-#'   y2 = LETTERS[3:1],
+#' d2 <- tibble(
+#'   x2 = c("a","b","b"),
+#'   y2 = c("B"),
 #'   b = rnorm(3)
 #' )
 #'
 #' left_join_m(d1, d2, by = c("x" = "x2", "y" = "y2"))
-
+#'
+#' d1 <- tibble(
+#'   x = c("a","b","b"),
+#'   y = c("B"),
+#'   a = rnorm(3)
+#' )
+#'
+#' d2 <- tibble(
+#'   x2 = letters[1:3],
+#'   y2 = LETTERS[1:3],
+#'   b = rnorm(3)
+#' )
+#'
+#' left_join_m(d1, d2, by = c("x" = "x2", "y" = "y2"))
 
 
 left_join_m <- function(df_left, df_right, by=NULL,... ) {
@@ -53,13 +67,13 @@ left_join_m <- function(df_left, df_right, by=NULL,... ) {
   )))
 
 
-  unmatched_left <- df_left %>% anti_join(df_right, by) %>% nrow()
+  unmatched_left <- df_left %>% {suppressMessages(anti_join(., df_right, by))} %>% nrow()
   message(paste0("Unmatched rows (left): ", unmatched_left))
 
-  temp <- key_left
-  names(temp) <- key_right
-  unmatched_right <- df_right %>% anti_join(df_left, by=temp) %>% nrow()
-  message(paste0("unmatched_right: ", unmatched_right))
+  # temp <- key_left
+  # names(temp) <- key_right
+  # unmatched_right <- df_right %>% {suppressMessages(anti_join(., df_left, by=temp))} %>% nrow()
+  # message(paste0("unmatched rows (right): ", unmatched_right))
 
   outcome_df
 
