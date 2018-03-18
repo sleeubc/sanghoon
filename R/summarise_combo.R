676a5dfa980466b552fd87f999fd30686de709f3
Sanghoon Lee <sanghoon.lee@sauder.ubc.ca>
2018-03-18 13:08:15 -0700

    Updated summarise_combo documentation.

--- R/summarise_combo.R
+++ R/summarise_combo.R
@@ -4,14 +4,20 @@
 #'
 #' https://stackoverflow.com/questions/28992028/grouping-over-all-possible-combinations-of-several-variables-with-dplyr/47949800#47949800
 #'
-#' @param
-#' @keywords summarise
+#' @param data data frame or tibble
+#' @param ... summarise commands
+#' @param type  If TRUE, it adds "_type_" column, an identifier for each group.
+#' @param na.str If TRUE, it convers NA to a string "NA" within each group. This distinguished "NA" as a group variable value from NA indicating the variable was not used as a group variable. See Examples.
+#' @keywords summarise, combo
 #' @export
 #' @examples
 #' library(tidyverse)
-#' mtcars %>% group_by(cyl, vs) %>% summarise_combo(cyl_n = n(), mean(mpg))
+#' data <- tibble(a = c("a1","a1","a2","a3"), b= c("b1", "b2", "b2", NA), c=c(1,2,3,4) )
+#' data %>% group_by(a,b) %>% summarise_combo(n(), mean(c))
+#' data %>% group_by(a,b) %>% summarise_combo(n(), mean(c), type=TRUE)
+#' data %>% group_by(a,b) %>% summarise_combo(n(), mean(c), type=TRUE, na.str=FALSE)
 
-summarise_combo <- function(data, ..., type=FALSE) {
+summarise_combo <- function(data, ..., type=FALSE, na.str=TRUE) {
 
   groupVarsStr <- group_vars(data)
   groupVarsNames <- groupVarsStr %>% map(as.name)
@@ -19,12 +25,24 @@ summarise_combo <- function(data, ..., type=FALSE) {
   groupCombos <-  map( 0:length(groupVarsNames), ~combn(groupVarsNames, ., simplify=FALSE) ) %>%
     unlist(recursive = FALSE)
 
-  results <- groupCombos %>%
-    map(function(x) {
-      # I need to replace NA with "NA". This is necessary; when results are combined, some NAs appear because they were not used as group_by variables.
-      replace_na_list <- rep("NA", length(x)) %>% as.list() %>% setNames(x)
-      data %>% group_by(!!! x) %>% summarise(...) %>% replace_na(replace_na_list)
+
+  if (isTRUE(na.str)) {
+
+    results <- groupCombos %>%
+      map(function(x) {
+        replace_na_list <- rep("NA", length(x)) %>% as.list() %>% setNames(x)
+        data %>% group_by(!!! x) %>% summarise(...) %>% replace_na(replace_na_list)
       } )
+
+
+  } else {
+
+    results <- groupCombos %>%
+      map(function(x) {
+        data %>% group_by(!!! x) %>% summarise(...)
+      } )
+
+  }
 
   if (isTRUE(type)) results <- results %>% map2( 1:length(results), ~ .x %>% mutate(`_type_` = .y) %>% select(`_type_`, everything())  )
 
