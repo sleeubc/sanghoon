c24b27e48e7c7c0d76329d03e48e4dadc7633ab7
Sanghoon Lee <sanghoon.lee@sauder.ubc.ca>
2022-03-23 20:52:10 -0700

    Change argument name in classify() from num_format to label

--- R/classify.R
+++ R/classify.R
@@ -5,16 +5,16 @@
 #' @param x input vector (numeric)
 #' @param n number of intervals
 #' @param style "equal", "quantile", "jenks"
-#' @param num_format a vectorized function that formats numbers.
+#' @param label a vectorized function that formats numbers.
 #' @keywords geom_sf, Jenks, natural breaks, equal interval, quantile, classification
 #' @export
 #' @examples
 #' library(ggplot2)
 #' library(sf)
 #' nc <- sf::st_read(system.file("shape/nc.shp", package = "sf"), quiet = TRUE)
-#' ggplot(nc) +  geom_sf(aes(fill = classify(AREA, 3, style ="jenks", num_format = scales::comma_format(accuracy = 0.01))))
+#' ggplot(nc) +  geom_sf(aes(fill = classify(AREA, 3, style ="jenks", label = scales::comma_format(accuracy = 0.01))))
 
-classify <- function(x, n, style="equal", num_format=scales::comma_format()) {
+classify <- function(x, n, style="equal", label=scales::comma_format()) {
 
   require(BAMMtools)
   require(scales)
@@ -27,7 +27,7 @@ classify <- function(x, n, style="equal", num_format=scales::comma_format()) {
                         stop("Style misspecified.")
   )
 
-  labels_cut <- paste( num_format(breaks_cut)[-length(breaks_cut)], num_format(lead(breaks_cut)[-length(breaks_cut)]), sep=" - ")
+  labels_cut <- paste( label(breaks_cut)[-length(breaks_cut)], label(lead(breaks_cut)[-length(breaks_cut)]), sep=" - ")
 
   cut( x, breaks= breaks_cut, labels=labels_cut )
 }
